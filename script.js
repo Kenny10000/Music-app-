687e52194314d69b3e6015b87efb71eea8537867
Kenny <bolajikehinde2468@gmail.com>
2024-01-31 15:26:09 +0100

    update

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ const fetchMe = () => {
                 <div class="text-center p-2">
                     <h5>${song.id}. ${song.songTitle} (ft) ${song.artistName}</h5>
                     <p>${song.releaseDate}</p>
-                    <div>
-                        <img src= '${song.songImage}' alt="dp" >
+                    <div class="container">
+                        <img src= '${song.songImage}' alt="dp">
+                        <div class="overlay">
+                        <div class="text-center text">Kenny music app <i class="i2 fa-solid fa-music animate__animated animate__bounce animate__heartBeat animate__infinite	infinite animate__delay-10s	15s"></i></div>
+                      </div>
                     </div>
                     <audio class="p-2" src='${song.songUrl}' controls/>
                     </div>
