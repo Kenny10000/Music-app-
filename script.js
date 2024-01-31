2246fe7ca20a7f4327d776ed37b3008449d18cdc
Kenny <bolajikehinde2468@gmail.com>
2024-01-31 11:59:51 +0100

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+const fetchMe = () => {
+    let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
+    fetch(url)
+        .then(res => res.json())
+        .then((conRes) => {
+            console.log(conRes);
+            conRes.map((song) => {
+                show.innerHTML += `
+                <div class="text-center">
+                    <h5>${song.id}. ${song.songTitle} (ft) ${song.artistName}</h5>
+                    <p>${song.releaseDate}</p>
+                    <div>
+                        <img src= '${song.songImage}' alt="dp" width="300"/>
+                    </div>
+                    <audio src='${song.songUrl}' controls/>
+                    </div>
+                    `
+             })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+
+
+
+
